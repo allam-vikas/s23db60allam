ce7887dd4734f24d264c5fdbe164e26c34fe49d9
allam-vikas <allamvikas1@gmail.com>
2023-11-09 23:57:25 -0600

    added the code for lab 12

--- controllers/vehicles.js
+++ controllers/vehicles.js
@@ -1,4 +1,5 @@
 var vehicles = require('../models/vehicles');
+//router.get('/vehicless/:id', vehicles_controller.vehicles_detail);
 // List of all vehicless
 // exports.vehicles_list = function(req, res) {
 // res.send('NOT IMPLEMENTED: vehicles list');
@@ -8,7 +9,6 @@ exports.vehicles_list = async function(req, res)
     try
     {
         thevehicless = await vehicles.find();
-    
         res.send(thevehicless);
     }
     catch(err)
@@ -17,24 +17,42 @@ exports.vehicles_list = async function(req, res)
         res.send(`{"error": ${err}}`);
     }
 };
-    
-// for a specific vehicles.
-exports.vehicles_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: vehicles detail: ' + req.params.id);
-};
-// Handle vehicles create on POST.
-exports.vehicles_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: vehicles create POST');
-};
+exports.vehicles_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await vehicles.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+ 
 // Handle vehicles delete form on DELETE.
 exports.vehicles_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: vehicles delete DELETE ' + req.params.id);
 };
-// Handle vehicles update form on PUT.
-exports.vehicles_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: vehicles update PUT' + req.params.id);
-};
-//VIEWS
+
+exports.vehicles_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await vehicles.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.name)
+    toUpdate.name = req.body.name;
+    if(req.body.color) toUpdate.cost = req.body.color;
+    if(req.body.mileage) toUpdate.size = req.body.mileage;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
+   //VIEWS
 // Handle a show all view
 exports.vehicles_view_all_Page = async function(req, res) {
     try{
